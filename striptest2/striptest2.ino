c4fdcd4bad7a0974970b83fc6b0527fb70bc8527
rpalmite <russell.palmiter@gmail.com>
2016-08-09 02:29:29 -0700

    added link as comment

--- striptest2/striptest2.ino
+++ striptest2/striptest2.ino
@@ -11,7 +11,7 @@
 //#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
 #include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
 
-#define NUMPIXELS 100 // Number of LEDs in strip
+#define NUMPIXELS 72 // Number of LEDs in strip
 
 // Here's how to control the LEDs from any two pins:
 #define DATAPIN    0
@@ -26,6 +26,10 @@ Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_
 // (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
 //Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BRG);
 
+// lower is faster
+//20 milliseconds (~50 FPS)
+#define SPEED_DELAY_MS 200
+
 void setup() {
 
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
@@ -34,7 +38,7 @@ void setup() {
 
   strip.begin(); // Initialize pins for output
 
-  strip.setBrightness(90); // 1/3 brightness
+  strip.setBrightness(20); // 1/3 brightness
 
   strip.show();  // Turn all LEDs off ASAP
 }
@@ -42,21 +46,68 @@ void setup() {
 // Runs 10 LEDs at a time along strip, cycling through red, green and blue.
 // This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
 
-int      head  = 0, tail = -2; // Index of first 'on' and 'off' pixels
-uint32_t color = 0xFF0000;      // 'On' color (starts red)
+#define TAIL_LENGTH 2
+uint32_t color = strip.Color(255, 0, 0);      // 'On' color (starts red)
+
+#define OFF_COLOR strip.Color(0, 0, 0)
+
+#define INTIAL_STEP 1
+int step = INTIAL_STEP;
+int direction = 1;
 
 void loop() {
-
-  strip.setPixelColor(head, color); // 'On' pixel at head
-  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
+  // refresh strip
+  strip.show();
   
-  strip.show();                     // Refresh strip
-  delay(20);                        // Pause 20 milliseconds (~50 FPS)
+  // pause (controls step of steps)
+  delay(SPEED_DELAY_MS);
 
-  if(++head >= NUMPIXELS) {         // Increment head index.  Off end of strip?
-    head = 0;                       //  Yes, reset head index to start
-    if((color >>= 8) == 0)          //  Next color (R->G->B) ... past blue now?
-      color = 0xFF0000;             //   Yes, reset to red
+  if (step == NUMPIXELS) {
+    //step = NUMPIXELS;
+    nextLine();
+    direction = -1;
+  } else if (step == 0)  {
+    //step = 1;
+    nextLine();
+    direction = 1;
   }
-  if(++tail >= NUMPIXELS) tail = 0; // Increment, reset tail index
+
+  nextStep();
+
+  step = step + direction; 
 }
+
+
+
+void nextStep() {
+  // move leds over 1 position
+
+  // turn head onto current color
+  strip.setPixelColor(step, color); // 'On' pixel at head
+
+  if (direction > 0) {
+      // turn off tail
+      strip.setPixelColor(step-TAIL_LENGTH, OFF_COLOR);     // 'Off' pixel at tail
+  }
+
+  if (direction < 0) {
+      strip.setPixelColor(step+TAIL_LENGTH, OFF_COLOR);     // 'Off' pixel at tail
+  }
+
+}
+
+
+void nextLine() {
+    // color change
+    if((color >>= 8) == 0)   {  //  Next color (R->G->B) ... past blue now?
+        color = strip.Color(255, 0, 0); // red
+    }
+
+}
+
+// color changing algorithm
+// http://codepen.io/Codepixl/pen/ogWWaK/
+// try changing the colors one step (led) at a time
+
+
+
